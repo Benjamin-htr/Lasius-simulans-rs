4eafc9de795d8cdfcfc478e8cc0c042b42140b59
Benjamin-htr <benjamin.htr42@gmail.com>
2026-05-23 11:19:10 +0200

    feat(movement): added basic movement on a triangle that move forward randomly

---
+++ src/features/ant/plugin.rs
@@ -0,0 +1,90 @@
+use std::f32::consts::FRAC_PI_2;
+
+use bevy::prelude::*;
+use rand::prelude::*;
+
+use bevy::window::PrimaryWindow;
+
+pub struct AntPlugin;
+
+impl Plugin for AntPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_ant);
+        app.add_systems(Update, movement_system);
+    }
+}
+
+#[derive(Component)]
+struct Ant;
+
+#[derive(Component)]
+struct Velocity {
+    x: f32,
+    y: f32,
+}
+
+impl Velocity {
+    fn zero() -> Self {
+        Self { x: 0.0, y: 0.0 }
+    }
+
+    fn to_vec2(&self) -> Vec2 {
+        Vec2::new(self.x, self.y)
+    }
+}
+
+fn spawn_ant(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    const TRIANGLE_SIZE: f32 = 12.0;
+
+    commands.spawn((
+        Ant,
+        Mesh2d(meshes.add(Triangle2d::new(
+            Vec2::new(0.0, TRIANGLE_SIZE * 1.5),
+            Vec2::new(-TRIANGLE_SIZE, -TRIANGLE_SIZE),
+            Vec2::new(TRIANGLE_SIZE, -TRIANGLE_SIZE),
+        ))),
+        MeshMaterial2d(materials.add(Color::srgb(1.0, 0.0, 0.0))),
+        Transform::default(),
+        Velocity::zero(),
+    ));
+}
+
+fn movement_system(
+    mut query: Query<(&mut Transform, &mut Velocity)>,
+    window_q: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window = window_q.single().unwrap();
+    let half_w = window.width() / 2.0;
+    let half_h = window.height() / 2.0;
+
+    let mut rng = rand::rng();
+
+    for (mut transform, mut velocity) in query.iter_mut() {
+        velocity.x += rng.random_range(-0.3..0.3);
+        velocity.y += rng.random_range(-0.3..0.3);
+
+        velocity.x = velocity.x.clamp(-2.0, 2.0);
+        velocity.y = velocity.y.clamp(-2.0, 2.0);
+
+        transform.translation.x += velocity.x;
+        transform.translation.y += velocity.y;
+
+        transform.rotation = Quat::from_rotation_z(velocity.to_vec2().to_angle() - FRAC_PI_2);
+
+        if transform.translation.x > half_w {
+            transform.translation.x = -half_w;
+        } else if transform.translation.x < -half_w {
+            transform.translation.x = half_w;
+        }
+
+        if transform.translation.y > half_h {
+            transform.translation.y = -half_h;
+        } else if transform.translation.y < -half_h {
+            transform.translation.y = half_h;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use bevy::prelude::*;
+
+mod features;
+use features::ant::AntPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(AntPlugin)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
 }
